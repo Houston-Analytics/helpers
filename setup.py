d204561537b7f03e1bfd709fb295ae49959cd373
Arttu Manninen <arttu@kaktus.cc>
2019-12-03 17:22:24 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ packages = setuptools.find_packages(
 
 setuptools.setup(
     name='helpers',
-    version='0.0.2',
+    version='0.0.3',
     author='Arttu Manninen',
     author_email='arttu@kaktus.cc',
     description=description,
